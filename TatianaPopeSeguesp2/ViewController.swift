8db19ba458e62d77528b2fcfc532bfe818bd0fa1
Tatiana Pope <tatianapope13@gmail.com>
2021-04-14 10:01:27 -0400

    Complete

--- TatianaPopeSeguesp2/ViewController.swift
+++ TatianaPopeSeguesp2/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            return segueSwitch.isOn
+        }
+    @IBAction func goToWhite(_ sender: UIButton) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "White", sender: nil)
+        }
+    }
+    
+    @IBAction func goToOrange(_ sender: UIButton) {
+        if segueSwitch.isOn {
+        performSegue(withIdentifier: "Orange", sender: nil)
+        }
+    }
+    
 }
 
